1e7b3d3f9fd376982b9afa3754c0ec5b136de8e3
VinhNguyen0301 <marsino16050301zeroone@gmail.com>
2022-12-11 21:27:33 +0700

    UI Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { Box, Stack } from "@mui/material";
 import NavBar from "./components/NavBar";
+import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
 
 function App() {
+  const { t, i18n } = useTranslation();
+  useEffect(() => {
+    const lng = navigator.language;
+    i18n.changeLanguage(lng);
+  }, []);
+
+  const lng = navigator.language;
   return (
     <Box bgcolor={"background.default"} color={"text.primary"}>
       <NavBar />
       <Stack direction="row" spacing={2} justifyContent="space-between">
         <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div>{lng}</div>
+        <div>{t("greeting.hello")}</div>
       </Stack>
       {/* <Add /> */}
     </Box>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,66 +16,66 @@ import Avatar from "@mui/material/Avatar";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import React, { useState } from "react";
 import { ChallengeIcon, InfoIcon, LogoIcon, ProfileIcon } from "./icon";
+import { useTranslation } from "react-i18next";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const NavBar = () => {
   const [open, setOpen] = useState(false);
+  const { t, i18n } = useTranslation();
+
   const StyledToolBar = styled(Toolbar)({
     display: "flex",
     justifyContent: "space-between",
     backgroundColor: "#414141",
   });
 
-  const Icons = styled(Box)(({ theme }) => ({
-    // backgroundColor: "white",
-    display: "none",
-    alignItems: "center",
-    gap: "20px",
-    [theme.breakpoints.up("sm")]: {
-      display: "flex",
-    },
-  }));
-
-  const UserBox = styled(Box)(({ theme }) => ({
-    // backgroundColor: "white",
-    display: "flex",
-    alignItems: "center",
-    gap: "10px",
-    [theme.breakpoints.up("sm")]: {
-      display: "none",
-    },
-  }));
-
   return (
     <AppBar position="sticky">
       <StyledToolBar>
-        <LogoIcon />
-
-        <ProfileIcon />
-        <ChallengeIcon />
-        <InfoIcon />
-        <UserBox onClick={(e) => setOpen(true)}>
-          <Avatar sx={{ width: "30px", height: "30px" }} />
-          <Typography>Mar Sino</Typography>
-        </UserBox>
+        <Box marginLeft="160px">
+          <LogoIcon />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            marginLeft: "288px",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <ProfileIcon />
+            <Typography>{t("myProfile")}</Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              marginLeft: "16px",
+            }}
+          >
+            <ChallengeIcon />
+            <Typography>{t("challenge")}</Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              marginLeft: "16px",
+            }}
+          >
+            <InfoIcon />
+            <Typography>{t("info")}</Typography>
+          </Box>
+        </Box>
+        <Box marginLeft="16px">
+          <MenuIcon />
+        </Box>
       </StyledToolBar>
-      <Menu
-        id="demo-positioned-menu"
-        aria-labelledby="demo-positioned-button"
-        open={open}
-        onClose={(e) => setOpen(false)}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-      >
-        <MenuItem>Profile</MenuItem>
-        <MenuItem>My account</MenuItem>
-        <MenuItem>Logout</MenuItem>
-      </Menu>
     </AppBar>
   );
 };
